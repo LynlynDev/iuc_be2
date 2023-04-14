2b8a3ff6de851d793071fabfe553c95022ffeeb6
LynlynDev <reinselynder@gmail.com>
2023-04-14 16:21:18 +0200

    Nouveau

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Style } from "../styles/Header.module.css";
+
+const Header = () => {
+    return (
+    <div className={Style.container}>
+        <h2>Dashboard</h2>
+        <h2>Welcome Back, Clint</h2>
+    </div>
+    
+    )
+}
+
+export default Header
---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import '@/styles/suna.css'
+import  { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
