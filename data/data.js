c86d2ae3274420ff32a026dd57db1fc1dc8ade66
LynlynDev <reinselynder@gmail.com>
2023-04-15 10:45:37 +0200

    Avancement 1.2

---
+++ data/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id:1,
+        name:{
+            first: 'Jean',
+            last: 'Smith',
+        },
+        email: 'jeansmith@gmail.com',
+        typeof: 'charger de filière',
+    },
+    {
+        id:2,
+        name:{
+            first: 'Sara',
+            last: 'Adams',
+        },
+        email: 'saraadams@gmail.com',
+        typeof: 'chef de département',
+    },
+    {
+        id:3,
+        name:{
+            first: 'Pierre',
+            last: 'Smith',
+        },
+        email: 'pierresmith@gmail.com',
+        typeof: 'responsable des EDT',
+    },
+    {
+        id:1,
+        name:{
+            first: 'Dorian',
+            last: 'Adams',
+        },
+        email: 'dorianadams@gmail.com',
+        typeof: 'charger de filière',
+    },
+]
